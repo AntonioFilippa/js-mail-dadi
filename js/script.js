0d11be6f1d3a447773f076c80b862479b026ab75
antoniofilippa94@gmail.com <antoniofilippa94@gmail.com>
2024-05-02 15:59:36 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+
+//ALERT DI BENVENUTO
+
+alert('Benvenuto')
+
+//CREAZIONE LISTA EMAIL
+
+let emailList = ['antoniofan@gmail.com', 'antoniofan1@gmail.com', 'antoniofan2@gmail.com']
+
+//RICHIESTA EMAIL DA UTENTE
